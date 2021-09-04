5fc20baa6729a551ec210ff8991cd7dc91b2e942
che711 <che711@mail.ru>
2021-09-04 13:51:47 +0300

    add test_swagger_store.py

--- tests_swagger_store.py
+++ tests_swagger_store.py
@@ -18,12 +18,12 @@ def test_store_oder(store_oder):
 def test_store_oder_id():
     '''Place an oder for a pet'''
     oder_id = requests.get(url=api_urls.Store.ODER_ID, headers=HEADERS)
-    print("\n\t", oder_id.url)
+    # print("\n\t", oder_id.url)
     assert oder_id.status_code == 200, 'Falling'
 
 
 def test_store_oder_delete():
     '''Delete an oder for a pet'''
     oder_delete = requests.delete(url=api_urls.Store.ODER_ID, headers=HEADERS)
-    print("\n\t", oder_delete.url)
+    # print("\n\t", oder_delete.url)
     assert oder_delete.status_code == 200, 'Falling'
